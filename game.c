5349186fd34f9b0bc847bb0f458f3349c59583f0
tramaine <lilmaine@gmail.com>
2013-11-05 23:35:06 -0700

    added functionality to movePlayer and created function called updatePlayer which updates the position of the player based off of the button pushed

--- game.c
+++ game.c
@@ -1,6 +1,6 @@
 #include <msp430g2553.h>
 #include "game.h"
-#include "LCD/LCD.h"
+#include "lcd.h"
 
 unsigned char initPlayer()
 {
@@ -22,13 +22,64 @@ void clearPlayer(unsigned char player)
 unsigned char movePlayer(unsigned char player, unsigned char direction)
 {
         switch (direction) {
-                //
-                // update player position based on direction of movement
-                //
+        case RIGHT:
+                if((player != 0x87) && (player != 0xC7)){
+                player += 0x01;
+                }
+                break;
+        case LEFT:
+                if(player != 0x80 && player != 0xC0){
+                player -= 0x01;
+                }
+                break;
+        case UP:
+                if(player > 0x87){
+                player -= 0x40;
+                }
+                break;
+        case DOWN:
+                if (player < 0xC0){
+                player += 0x40;
+                }
+                break;
         }
 
         return player;
 }
+
+void updatePlayer(unsigned char player){ //update the position of the player based off the button pushed
+	if(isP1ButtonPressed(BIT1))
+	        {
+	                            clearPlayer(player);
+	                            player = movePlayer(player, RIGHT);
+	                            printPlayer(player);
+	                            waitForP1ButtonRelease(BIT1);
+	                            debounce();
+	                    }
+	                    else if(isP1ButtonPressed(BIT2)){
+	                            clearPlayer(player);
+	                            player = movePlayer(player, LEFT);
+	                            printPlayer(player);
+	                            waitForP1ButtonRelease(BIT2);
+	                            debounce();
+	                    }
+	                    else if(isP1ButtonPressed(BIT3)){
+	                            clearPlayer(player);
+	                            player = movePlayer(player, UP);
+	                            printPlayer(player);
+	                            waitForP1ButtonRelease(BIT3);
+	                            debounce();
+	                    }
+	                    else if(isP1ButtonPressed(BIT4)){
+	                            clearPlayer(player);
+	                            player = movePlayer(player, DOWN);
+	                            printPlayer(player);
+	                            waitForP1ButtonRelease(BIT4);
+	                            debounce();
+	                    }
+}
+
+
 
 char didPlayerWin(unsigned char player)
 {
